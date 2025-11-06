1e568e982c60e5099d5d61e07de74c559f9f986e
Kenosun <halloichbins1211@gmail.com>
2025-11-06 19:19:17 +0100

    Add media listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use windows::{Media::Control::GlobalSystemMediaTransportControlsSessionManager, core::Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let global_media_manager = GlobalSystemMediaTransportControlsSessionManager::RequestAsync()?;
+    let manager = global_media_manager.await?;
+
+    let mut last_track = None;
+
+    loop {
+        if let Ok(session) = manager.GetCurrentSession() {
+            if let Ok(media_props) = session.TryGetMediaPropertiesAsync() {
+                if let Ok(media) = media_props.await {
+                    let artist = media.Artist()?.to_string_lossy();
+                    let title = media.Title()?.to_string_lossy();
+                    let current_track = format!("{} - {}", artist, title);
+
+                    if last_track != Some(current_track.clone()) {
+                        println!("Now playing: {}", current_track);
+                        last_track = Some(current_track)
+                    }
+                }
+            }
+        }
+    }
 }
