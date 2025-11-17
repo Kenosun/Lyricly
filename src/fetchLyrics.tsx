81059c16e5928e7ce494b3e8886564f76faa2d34
Kenosun <halloichbins1211@gmail.com>
2025-11-17 22:49:07 +0100

    Fix duration not being in seconds

--- src/fetchLyrics.tsx
+++ src/fetchLyrics.tsx
@@ -1,11 +1,14 @@
-export async function fetchLyricsResponse(
+export async function fetchLyrics(
   artist: string,
   title: string,
   duration: number
 ): Promise<{ lyricsResponse: any; synced: boolean } | null> {
   const encodedArtist = encodeURIComponent(artist);
   const encodedTitle = encodeURIComponent(title);
-  const url = `https://lrclib.net/api/search?artist_name=${encodedArtist}&track_name=${encodedTitle}&duration=${duration}`;
+  const durationInSeconds = duration / 1000;
+  const url = `https://lrclib.net/api/search?artist_name=${encodedArtist}&track_name=${encodedTitle}&duration=${durationInSeconds}`;
+
+  console.log(url);
 
   try {
     const response = await fetch(url);
